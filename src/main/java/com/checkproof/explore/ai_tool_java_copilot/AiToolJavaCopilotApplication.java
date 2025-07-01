82bf50061d285088fe1aefca49952026e2b8298a
rully <rully.kurniawan@checkproof.com>
2025-07-01 14:48:06 +0700

    initial commit

---
+++ src/main/java/com/checkproof/explore/ai_tool_java_copilot/AiToolJavaCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.checkproof.explore.ai_tool_java_copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolJavaCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiToolJavaCopilotApplication.class, args);
+	}
+
+}
